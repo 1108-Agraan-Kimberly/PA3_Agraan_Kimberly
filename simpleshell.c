75039e62ce7d00bfe244f58bbae88b5782743d20
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2025-09-18 01:30:48 +0000

    functions there

---
+++ simpleshell.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int executeCommand(char* []);
+void changeDirectories(char*);
+int parseInput(char*, char* []);
+
+int main(){
+
+
+    return 0;
+}
+
+int executeCommand(char* []){
+
+
+}
+
+void changeDirectories(char*){
+
+
+}
+
+int parseInput(char*, char* []){
+
+    
+}
