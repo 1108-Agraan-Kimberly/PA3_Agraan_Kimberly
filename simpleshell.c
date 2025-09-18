04637e02c877352e371889548b7890c4925a14f7
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2025-09-17 22:10:16 -0700

    errono

--- simpleshell.c
+++ simpleshell.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <errno.h>
 
-int executeCommand(char* []);
-void changeDirectories(char*);
-int parseInput(char*, char* []);
+int executeCommand(char* const*, const char*, const char*);
+void changeDirectories(const char*);
+int parseInput(char*, char [], int);
 
 int main(){
 
@@ -12,17 +15,28 @@ int main(){
     return 0;
 }
 
-int executeCommand(char* []){
-
-
-}
-
-void changeDirectories(char*){
-
-
-}
-
-int parseInput(char*, char* []){
+int executeCommand(char* const* enterCommand, const char* infile, const char* outfile){
 
     
+
+}
+
+void changeDirectories(const char* path){
+
+    if(chdir(path) != 0){
+        printf("error code: %d\n", errno);
+        printf("error: %s\n", strerror(errno));
+    }
+
+}
+
+int parseInput(char* input, char splitWords[], int maxWords){
+    strtok(input, " \n");
+    while(input != NULL && maxWords > 0){
+        strcpy(splitWords, input);
+        splitwords += strlen(input) + 1;
+        maxWords--;
+        input = strtok(NULL, " \n")
+    }
+    return maxWords;
 }
