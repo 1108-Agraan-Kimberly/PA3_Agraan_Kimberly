531f2fee255d082c0ca04ecbb3f0150072f47ad7
1108-Agraan-Kimberly <143475961+1108-Agraan-Kimberly@users.noreply.github.com>
2025-10-09 17:46:27 -0700

    changeUSM

--- sched.c
+++ sched.c
@@ -8,6 +8,7 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "print_progress.c"
 
 typedef struct _thread_data_t {
 int localTid;
@@ -26,8 +27,34 @@ int main(int argc, char* argv[]){
 
 }
 
-void* arraySum(void*){
+void* arraySum(void* arg){
 
+    thread_data_t* tdata = (thread_data_t*) arg;
 
+    while(1){
+        long long sum = 0;
+        long latencyMax = 0;
+
+        for(int i =0; i < tdata->numVals; i++){
+            struct timespec start end;
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            threadSum += tdata->data[i];
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            long latency = (end.tv_sec - start.tv_sec) * 1000000000L + (end.tv_nsec - start.tv_nsec);
+            
+            if(latency > latencyMax){
+                latencyMax = latency;
+            }
+        }
+
+        pthread_mutex_lock(tdata->lock);
+        *(tdata->totalSum) += sum;
+        pthread_mutex_unlock(tdata->lock);
+        print_progress(tdata->localTid, latencyMax);
+
+    }
+    
+    return NULL;
     
 }
